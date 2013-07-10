bb54b1226771afaac10585f02c1f8c6771f96cc8
Hugh Nguyen <hugh.hn@gmail.com>
2013-07-10 21:44:15 +0000

    touch web.js

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var buf = fs.readFileSync('index.html');
-var indexStr = buf.toString("utf-8",0,27);
+var indexStr = buf.toString('utf-8',0,27);
 
 app.get('/', function(request, response) {
   response.send(indexStr);
